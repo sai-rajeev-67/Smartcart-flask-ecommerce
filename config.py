4c221e24e94391315f80eba1ef0429fc57c13ee2
Medisetti Sai Rajeev Lochan <medesettysai@gmail.com>
2026-07-10 17:48:18 +0530

    Added DB_PORT support for Railway MySQL

--- config.py
+++ config.py
@@ -8,10 +8,13 @@ import os
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # MySQL Database
+# MySQL Database
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+
 
 # Email Settings
 MAIL_SERVER = 'smtp.gmail.com'
